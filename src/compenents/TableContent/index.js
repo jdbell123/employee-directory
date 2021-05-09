5b6d4c442c51f4a3be5005620000bc7d16104727
John Bell <jdbell123@gmail.com>
2021-05-09 19:25:35 -0400

    split first and last name for sorting

--- src/compenents/TableContent/index.js
+++ src/compenents/TableContent/index.js
@@ -5,7 +5,8 @@ function TableContent(props) {
         <tbody>
         <tr>
           <td className="image text-center"><img alt={props.alt} src={props.src} className="img-fluid" /></td>
-          <td className="name text-center pt-4">{props.firstName} {props.lastName}</td>
+          <td className="first_name text-center pt-4">{props.firstName}</td>
+          <td className="last_name text-center pt-4">{props.lastName}</td>
           <td className="email text-center pt-4">{props.emailAddress}</td>
           <td className="phone text-center pt-4">{props.phoneNumber}</td>
           <td className="dob text-center pt-4">{props.dob}</td>
