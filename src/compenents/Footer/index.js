530b543dcf1de8ea323bd9cfe69e90c66ece1357
John Bell <jdbell123@gmail.com>
2021-05-09 16:02:55 -0400

    added footer compenents

---
+++ src/compenents/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+  return (
+    <footer className="footer bg-primary text-white">
+      <span>John Bell 2021</span>
+    </footer>
+  );
+}
+
+export default Footer;
