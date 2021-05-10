c8abebbd0b47313ebaf4332a9b098782e474bc87
John Bell <jdbell123@gmail.com>
2021-05-09 20:39:03 -0400

    Changed wording about sorting

--- src/compenents/NavBar/index.js
+++ src/compenents/NavBar/index.js
@@ -19,7 +19,7 @@ function Navbar() {
       <div className="jumbotron jumbotron-fluid text-center bg-primary mb-0">
         <div className="header container">
           <h1 className="display-4 font-weight-bold text-white">Employee Directory</h1>
-          <p className="lead text-white">Click on carrots to sort by heading or use the search box to narrow/filter your results.</p>
+          <p className="lead text-white">Click on the column headers to sort the data in that column or use the search box to narrow/filter your results.</p>
         </div>
       </div>
       <ColoredLine color="red" />
